19a22a86cd4ad856aa0602592894aa14e50849f7
Nandazzz <123712584+Nandazzz@users.noreply.github.com>
2023-05-26 17:46:46 +0700

    fixing storage

--- src/storage/localStorage.js
+++ src/storage/localStorage.js
@@ -1,16 +1,13 @@
 const TODO_KEY = "todo-list";
 
-//Set localStorage to exist
 const storageExist = (data) => {
   if (!localStorage) {
     console.log("browser tidak support local storage");
   } else {
-    //Call saveData and send object value to argument
     saveData(data);
   }
 };
 
-//Add items to localStorage
 const saveData = (data) => {
   localStorage.setItem(TODO_KEY, JSON.stringify(data));
 };
